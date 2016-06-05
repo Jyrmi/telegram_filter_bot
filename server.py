6bc20f554448c0a2878100dfe30f307feb7e6910
Joseph Choi <chousemath@gmail.com>
2016-06-05 11:14:00 +0900

    changed port 4

--- server.py
+++ server.py
@@ -1,53 +1,12 @@
-import telegram
-import os
-from flask import Flask, request
+#!flask/bin/python
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-@app.route('/alternate')
-def hello_alternate():
-    return 'Hello from the alternate universe!!!!'
-
-# Define a few command handlers. These usually take the two arguments bot and
-# update. Error handlers also receive the raised TelegramError object in error.
-def start(update):
-    """
-    Start the party.
-
-    This function gets da partaaaaayyy started
-    Only activated when a new conversation is started with this bot
-    """
-    message = 'Hi! Do you need /help? Check out my /filters!'
-    bot.sendMessage(update.message.chat_id, text=message)
-
-
-@app.route('/HOOK', methods=['POST'])
-def webhook_handler():
-    if request.method == "POST":
-        # retrieve the message in JSON and then transform it to Telegram object
-        update = telegram.Update.de_json(request.get_json(force=True))
-
-        chat_id = update.message.chat.id
-
-        # Telegram understands UTF-8, so encode text for unicode compatibility
-        text = update.message.text.encode('utf-8')
-
-        if text == '/start':
-            start(update)
-
-        else:
-            # repeat the same message back (echo)
-            bot.sendMessage(chat_id=chat_id, text=text)
-
+def index():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    bot.setWebhook('https://shrouded-everglades-90342.herokuapp.com/HOOK')
-    bot = telegram.Bot(token=os.environ['TELEGRAM_KEY'])
-    # bot.setWebhook('https://telegram-filter-bot.herokuapp.com/HOOK')
     port = int(os.getenv('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
