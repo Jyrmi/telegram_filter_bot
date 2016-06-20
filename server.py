1e79c004d41763b35dcf904c7350353b4902d4fa
Jyrmi <jeremyschoi@gmail.com>
2016-06-20 16:30:39 -0400

    make webhook url consistent with app route

--- server.py
+++ server.py
@@ -429,7 +429,8 @@ def echo(bot, update):
 
 # @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-    s = bot.setWebhook('https://telegram-filter-bot.herokuapp.com/HOOK')
+    s = bot.setWebhook('https://telegram-filter-bot.herokuapp.com/')
+    # s = bot.setWebhook('https://telegram-filter-bot.herokuapp.com/HOOK')
     # s = bot.setWebhook('https://pacific-dusk-98067.herokuapp.com/HOOK')
     if s:
         return "webhook setup ok"
