dc88326c42fc5bb5814c4d1152b186a65e8daa5a
Joseph Choi <chousemath@gmail.com>
2016-08-08 23:24:10 +0900

    could not get circle crop to work, commented out all circle code

--- server.py
+++ server.py
@@ -121,17 +121,17 @@ def filter_image(bot, update):
             img = ImageOps.invert(img)
             applied_filters.append('inverted')
 
-        elif 'circle' in f:
-            mask = Image.open('./mask_1.png').convert('L')
-            img = ImageOps.fit(img, mask.size, centering=(0.5, 0.5))
-            img.putalpha(mask)
-            # applied_filters.append('circle')
-            print('before save okay')
-            img.save(chat_id+'/filtered.png')
-            print('after save okay')
-            bot.sendDocument(update.message.chat_id,
-                             document=open(chat_id+'/filtered.png', 'rb'))
-            print('after send okay')
+        # elif 'circle' in f:
+        #     mask = Image.open('./mask_1.png').convert('L')
+        #     img = ImageOps.fit(img, mask.size, centering=(0.5, 0.5))
+        #     img.putalpha(mask)
+        #     # applied_filters.append('circle')
+        #     print('before save okay')
+        #     img.save(chat_id+'/filtered.png')
+        #     print('after save okay')
+        #     bot.sendDocument(update.message.chat_id,
+        #                      document=open(chat_id+'/filtered.png', 'rb'))
+        #     print('after send okay')
         # The specified filter is one of the ImageFilter module ones
         elif f in filters:
             img = img.filter(filters[f])
